498252f8620d78076b04b0004bfe5eea79162298
Banu Ağgün <banuaggun@outlook.com>
2026-05-14 21:55:30 +0300

    feat(heroCards): add VisionCard component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react' 
 import "./hero.css";
+import VisionCard from '../heroCards/vision-card/VisionCard';
 
 const Hero = () => {
   return (
     <div className='hero'>
         <div className="hero-content">
-          
+          <VisionCard />
         </div>
     </div>
   )
---
+++ src/components/heroCards/vision-card/VisionCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./vision.css";
+
+
+const VisionCard = () => {
+  return (
+    <div className='vision'>
+        <div className='vision-content'>
+            <h2>Advanced <span>e-commerce</span> <br/> analytics products </h2> 
+            <p>Quo, neque minima! In laborum, mollitia et repellat, tenetur quibusdam cum cumque a sequi eveniet maxime voluptatibus quia!</p>
+        </div>
+    </div>
+  )
+}
+
+export default VisionCard
