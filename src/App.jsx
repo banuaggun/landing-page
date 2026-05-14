4b2cd52a492d2520337d92ec71d3aec8b75d4e96
Banu Ağgün <banuaggun@outlook.com>
2026-05-14 21:54:51 +0300

    feat(hero): add Hero component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,7 @@ function App() {
     <>
       <section>
         <Navbar /> 
-        
+        <Hero />
       </section>
 
       
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-content">
+          
+        </div>
+    </div>
+  )
+}
+
+export default Hero
