50146705b64e9eae6cc88e1603cab3e5798d3581
Banu Ağgün <banuaggun@outlook.com>
2026-05-15 21:17:28 +0300

    feat(hero): integrate card components

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,15 +1,24 @@
-import React from 'react' 
+import React from "react";
 import "./hero.css";
-import VisionCard from '../heroCards/vision-card/VisionCard';
+import VisionCard from "../heroCards/vision-card/VisionCard";
+import GoalCard from "../heroCards/goal-card/GoalCard";
+import ServiceCard from "../heroCards/service-card/ServiceCard";
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <div className="hero-content">
+    <div className="wrapper">
+      <div className="hero">
+        <div className="hero-left">
           <VisionCard />
         </div>
+        <div className="hero-right">
+          <GoalCard />
+          <ServiceCard />
+        </div>
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
