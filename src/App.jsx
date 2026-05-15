45f33e0fd7f998d7a94396528679af61b5946ed8
Banu Ağgün <banuaggun@outlook.com>
2026-05-15 21:14:10 +0300

    feat: add Navbar & Hero components and update CSS structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Navbar from './components/navbar/Navbar'
-import Hero from './components/hero/Hero'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <section>
-        <Navbar /> 
-        <Hero />
+      <section> 
+        <div className="nav-bar">
+    <Navbar />
+        </div>
+    
+        <div className="content">
+          <Hero />
+        </div>
       </section>
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
