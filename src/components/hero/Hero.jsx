6b3ef10b63fb289d3870b6bc840c145e35a4bc59
Banu Ağgün <banuaggun@outlook.com>
2026-05-18 08:12:42 +0300

    feat(hero): integrate card components - 2

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,6 +3,9 @@ import "./hero.css";
 import VisionCard from "../heroCards/vision-card/VisionCard";
 import GoalCard from "../heroCards/goal-card/GoalCard";
 import ServiceCard from "../heroCards/service-card/ServiceCard";
+import TeamCard from "../heroCards/team-card/TeamCard";
+import StatsCard from "../heroCards/stats-card/StatsCard";
+import RevenueCard from "../heroCards/revenue-card/RevenueCard";
 
 const Hero = () => {
   return (
@@ -16,7 +19,15 @@ const Hero = () => {
           <ServiceCard />
         </div>
       </div>
-      
+      <div className="hero">
+        <div className="hero-left">
+          <TeamCard />
+        </div>
+        <div className="hero-right ">
+          <StatsCard />
+          <RevenueCard />
+        </div>
+      </div>
     </div>
   );
 };
