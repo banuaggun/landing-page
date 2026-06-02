364ab8f75f7a1f9927881cbeeb2761b05c96ab55
Banu Ağgün <banuaggun@outlook.com>
2026-06-02 21:23:02 +0300

    cleanup: deleted all components to rebuild landing page from scratch

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,31 +1,12 @@
 import React from "react";
 import "./hero.css";
-import VisionCard from "../heroCards/vision-card/VisionCard";
-import GoalCard from "../heroCards/goal-card/GoalCard";
-import ServiceCard from "../heroCards/service-card/ServiceCard";
-import TeamCard from "../heroCards/team-card/TeamCard";
-import StatsCard from "../heroCards/stats-card/StatsCard";
-import RevenueCard from "../heroCards/revenue-card/RevenueCard";
 
 const Hero = () => {
   return (
     <div className="wrapper">
       <div className="hero">
         <div className="hero-left">
-          <VisionCard />
-        </div>
-        <div className="hero-right">
-          <GoalCard />
-          <ServiceCard />
-        </div>
-      </div>
-      <div className="hero">
-        <div className="hero-left">
-          <TeamCard />
-        </div>
-        <div className="hero-right ">
-          <StatsCard />
-          <RevenueCard />
+          deneme
         </div>
       </div>
     </div>
