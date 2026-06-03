839bfe24f62668dbe12d02474237269852f58617
Banu Ağgün <banuaggun@outlook.com>
2026-06-03 15:56:40 +0300

    feat(layout): set up main page structure with fixed navbar and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { useState } from "react"; 
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Products from "./components/products/Products";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <section> 
+     
         <div className="nav-bar">
     <Navbar />
         </div>
-    
-        <div className="content">
-          <Hero />
-        </div>
+    <main>
+      <section id="home" className="content">
+        <Hero />
+      </section> 
+
+      <section id="products">
+        <Products />
       </section>
+    </main>
+        
     </>
   );
 }
