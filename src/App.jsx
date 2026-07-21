ead6926010e981c4db4a29e333dc02505c57c155
Banu Ağgün <banuaggun@outlook.com>
2026-07-21 20:52:19 +0300

    style: mini optimizing

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,11 @@ function App() {
           <Support />
           <Reinforce/>
         </section>
-
+{/*
         <section id="products">
           <Products />
         </section>
+        */}
       </main> 
       <Footer/>
     </>
