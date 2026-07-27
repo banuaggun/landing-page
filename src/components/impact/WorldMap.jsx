09939d777dd359ca1b643d25fac377061eebfdee
Banu Ağgün <banuaggun@outlook.com>
2026-07-27 14:14:47 +0300

    feat(ui): optimize interactive map for all screen sizes and hover states

--- src/components/impact/WorldMap.jsx
+++ src/components/impact/WorldMap.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import exploreIcon from '../../assets/global-impact/explore-icon.svg';
-import { Map } from './Map'; 
-import './impact.css';
+import React from "react";
+import exploreIcon from "../../assets/global-impact/explore-icon.svg";
+import { Map } from "./Map";
+import "./impact.css";
 
 const WorldMap = ({ setActiveProject }) => {
   return (
-    <div className="map-wrapper"> 
-      <div className="map-area">
-  
-        <Map className="map-image" onPinClick={setActiveProject} /> 
-      </div>
-
+    <div className="map-wrapper">
       <div className="mobile-map-guide">
         <div className="guide-icon-wrapper">
           <img src={exploreIcon} alt="Explore" />
         </div>
-        <p>
-          <strong>Explore Projects:</strong> Tap on any pin to view forest details.
+        <p className="fonts-body">
+          <strong>Explore Projects:</strong> Tap on any pin to view forest
+          details.
         </p>
+      </div>
+      <div className="map-area">
+        <Map className="map-image" onPinClick={setActiveProject} />
       </div>
     </div>
   );
